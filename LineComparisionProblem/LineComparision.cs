fa54efc71b993e225815689b5d83875ecb04c07d
saikiranreddy <saikiranreddy@gmail.com>
2022-04-11 17:06:55 +0530

    [kiran]-Added UC1 in LineComparision

---
+++ LineComparisionProblem/LineComparision.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    internal class LineComparision
+    {
+        public double x1, x2, y1, y2, length;
+        public void LineLength()
+        {
+            Console.WriteLine("Please Enter x , y Co-ordinates of a line :");
+            Console.WriteLine("Please Enter values of x1 and y1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter values of x2 and y2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+
+            length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of the line is : " + length);
+        }
+    }
+}
---
+++ LineComparisionProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComparision lineComparision = new LineComparision();
+            lineComparision.LineLength();
+            Console.ReadLine();
+        }
+    }
+}
